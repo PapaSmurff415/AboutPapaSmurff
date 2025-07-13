54d2cb2353448c22933b1f966d118141de7deb6b
Papa_Smurff415 <Cimkannakan@gmail.com>
2025-07-13 08:01:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello visitor");
+console.log("[+] Welcome to my terminal portfolio.");
+console.log("[!] Everything you see here is a work of skill and passion.");
+function zoomIn(img) {
+    img.style.transform = "scale(2)";
+    img.style.transition = "transform 0.3s ease";
+}
